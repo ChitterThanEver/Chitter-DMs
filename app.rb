c889afb53fb3bf52993f1a80d2da866db6f0c52b
Jon Pillay <jonpillay@MASTUDENT-Jon-Pillay-C02G5H47Q6L4.local>
2023-04-13 16:14:16 +0100

    homepage, log in and log out

--- app.rb
+++ app.rb
@@ -1,14 +1,46 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative './lib/user_repository'
+require 'sinatra/flash'
+require_relative './lib/database_connection'
+
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
   # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+    register Sinatra::Flash
+    enable :sessions
+    also_reload './lib/user_repository'
+  end
+
+  def check_handle_exists(handle)
+    user_repo = UserRepository.new
+    if user_repo.list_handles.include?(handle)
+      return true
+    end
+  end
+
+  post '/login' do
+    handle = params[:handle]
+    if check_handle_exists(handle)
+      session[:handle] = handle
+      redirect '/'
+    else
+      flash[:invalid_handle] = "Handle Does Not Exist"
+      redirect '/'
+    end
+  end
+
+  get '/logout' do
+    session[:handle] = nil
+    redirect '/'
   end
 
   get '/' do
-    "Hello World!"
+    @user_repo = UserRepository.new
+    return erb(:index)
   end
 end
---
+++ spec/integration/app_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'rack/test'
+require_relative '../../app'
+
+describe Application do
+  include Rack::Test::Methods
+
+  let(:app) { Application.new }
+
+  def reset_tables
+    seed_sql = File.read('spec/seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter-dms-test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_tables
+  end
+
+  context "GET /" do
+    it "returns the homepage with list of DMs if user is logged in" do
+      response = get("/")
+      expect(response.status).to eq 200
+      expect(response.body).to include '<h2>You are not logged in<h2>'
+
+      response = post("/login", handle: 'Bob')
+      expect(response.status).to eq 302
+
+      response = get("/")
+      expect(response.status).to eq 200
+      expect(response.body).to include 'You are logged in'
+    end
+
+    it "returns homepage with log in form if user is not logged in" do
+      response = get("/")
+
+      expect(response.status).to eq 200
+      expect(response.body).to include '<h2>You are not logged in<h2>'
+    end
+  end
+end
