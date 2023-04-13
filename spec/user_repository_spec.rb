4bdca9de0f660bd811ad29d005fe4ea5bbd66a35
Pablo <pablito@me.com>
2023-04-13 15:54:52 +0100

    Updated user_repository spec and renamed user file

--- spec/user_repository_spec.rb
+++ spec/user_repository_spec.rb
@@ -19,6 +19,33 @@ describe UserRepository do
       expect(users.length).to eq(7)
       expect(users[-1].handle).to eq(7)
 
+      #####
+      expect(users[-1].handle).to eq('Lucy')
+      expect(users[-1].verified).to eq(false)
+      expect(users[0].handle).to eq('Bob')
+      expect(users[0].verified).to eq(false)
     end
   end
+
+  context "#find_handle" do
+    it "finds the handle based on id" do
+      repo = UserRepository.new
+      user = repo.find_handle(1)
+
+      expect(user.handle).to eq('Bob')
+    end
+
+    it "finds the handle based on id" do
+      repo = UserRepository.new
+      user = repo.find_handle(4)
+
+      expect(user.handle).to eq('Mary')
+    end
+  end
+
+  context "#find_blocked" do
+
+
+
+
 end
